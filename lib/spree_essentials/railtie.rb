507a55f7e8fc0c505472db3e58ee8f8b89dd0f2c
André Barbosa <albmail88@gmail.com>
2013-08-13 18:24:35 +0100

    fixed bug that was preventing from startign the server

--- lib/spree_essentials/railtie.rb
+++ lib/spree_essentials/railtie.rb
@@ -2,7 +2,6 @@ module SpreeEssentials
   class Railtie < Rails::Railtie
     initializer "spree_essentials.spree_helpers" do
       ActionView::Base.send :include, Spree::StoreHelper
-      ActionView::Base.send :include, Spree::ControllerHelpers::Order
     end
   end
 end
